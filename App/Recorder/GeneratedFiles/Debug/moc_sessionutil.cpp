546260f8b328f4b320d88490e6d04d626e44020c
VanceKingSaxbeA <vsaxbe@yahoo.com>
2014-02-23 23:32:00 +0530

    3654 th/nd times commit by Vance King Saxbe. A and development team Power Dominion Enterprise

--- App/Recorder/GeneratedFiles/Debug/moc_sessionutil.cpp
+++ App/Recorder/GeneratedFiles/Debug/moc_sessionutil.cpp
@@ -1,4 +1,4 @@
-/****************************************************************************
+/* Copyright (c) <2014> Author Vance King Saxbe. A, and contributors Power Dominion Enterprise, Precieux Consulting and other contributors. Modelled, Architected and designed by Vance King Saxbe. A. with the geeks from GoldSax Consulting and GoldSax Technologies email @vsaxbe@yahoo.com. Development teams from Power Dominion Enterprise, Precieux Consulting. Project sponsored by GoldSax Foundation, GoldSax Group and executed by GoldSax Manager.*//****************************************************************************
 ** Meta object code from reading C++ file 'sessionutil.h'
 **
 ** Created: Thu Sep 26 13:47:03 2013
--- App/Recorder/src/replace.cpp
+++ App/Recorder/src/replace.cpp
@@ -1,4 +1,4 @@
-#include "replace.h"
+/* Copyright (c) <2014> Author Vance King Saxbe. A, and contributors Power Dominion Enterprise, Precieux Consulting and other contributors. Modelled, Architected and designed by Vance King Saxbe. A. with the geeks from GoldSax Consulting and GoldSax Technologies email @vsaxbe@yahoo.com. Development teams from Power Dominion Enterprise, Precieux Consulting. Project sponsored by GoldSax Foundation, GoldSax Group and executed by GoldSax Manager.*/#include "replace.h"
 #include "eventeditor.h"
 #include "textedit.h"
 Replace::Replace(QWidget *parent)
